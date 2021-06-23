22a76562191cf51f0e8e6bbf675d7005b18df3f9
Alexander Yalov <alexander.yalov@gmail.com>
2021-06-23 23:16:53 +0300

    remove index of moduleInventoryPart, add numbers of parts and slots

--- HideInventorySlots/HideInventorySlots.cs
+++ HideInventorySlots/HideInventorySlots.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using KSP.Localization;
 
 namespace HideInventorySlots
 {
@@ -13,25 +14,51 @@ namespace HideInventorySlots
         public void Start()
         {
             GameEvents.onPartActionUIShown.Add(OnPartActionWindowCreated);
+            GameEvents.onModuleInventoryChanged.Add(OnModuleInventoryChanged);
         }
+
+
+        private void OnModuleInventoryChanged(ModuleInventoryPart moduleInventoryPart)
+        {
+            //Debug.Log("OnModuleInventoryChanged");
+            String name = moduleInventoryPart.Fields["InventorySlots"].group.name;
+
+            UIPartActionWindow window = UIPartActionController.Instance.GetItem(moduleInventoryPart.part, false);
+            if (window != null)
+            {
+                UIPartActionGroup group = window.parameterGroups[name];
+                group.Initialize(name, GroupDisplayName(moduleInventoryPart), false, window);
+            }
+        }
+
 
         private void OnPartActionWindowCreated(UIPartActionWindow window, Part part)
         {
             List<UIPartActionItem> items = window.ListItems.Where(item => item.PartModule is ModuleInventoryPart).ToList();
-
             for (int i = 0; i < items.Count; i++)
             {
                 ModuleInventoryPart moduleInventoryPart = items[i].PartModule as ModuleInventoryPart;
-                BasePAWGroup group = new BasePAWGroup($"{i}", $"Inventory {(items.Count == 1 ? string.Empty : $"{i}")}", true);
+
+                BasePAWGroup group = new BasePAWGroup($"{i}", GroupDisplayName(moduleInventoryPart), false);
                 moduleInventoryPart.Fields["InventorySlots"].group = group;
             }
 
             window.displayDirty = true;
         }
 
+
+        String GroupDisplayName(ModuleInventoryPart moduleInventoryPart, int? index = null)
+        {
+            return String.Format("{0}{1} ({2}/{3})", Localizer.Format("#autoLOC_8320000"),
+                    index is null ? string.Empty : " " + index,
+                    moduleInventoryPart.InventoryItemCount, moduleInventoryPart.InventorySlots);
+        }
+
+ 
         public void OnDestroy()
         {
             GameEvents.onPartActionUIShown.Remove(OnPartActionWindowCreated);
+            GameEvents.onModuleInventoryChanged.Remove(OnModuleInventoryChanged);
         }
     }
 }
